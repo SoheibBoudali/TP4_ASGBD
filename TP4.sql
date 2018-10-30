f084810dac13b3663854cc4ce57ecf09e7e191fb
SoheibBoudali <39391107+SoheibBoudali@users.noreply.github.com>
2018-10-30 20:43:34 +0100

    procédure qui permet d’ajouter un employé de type infirmier à partir de tous les attributs nécessaires

--- TP4.sql
+++ TP4.sql
@@ -89,3 +89,50 @@ BEGIN
 END Med_Spec;
 
 select Med_Spec('ORTHOPEDISTE') from dual;
+select Med_Spec('CARDIOLOGUE') from dual;
+
+/* 5  procédure qui permet d’ajouter un employé de type infirmier à partir de tous les attributs nécessaires*/
+CREATE OR REPLACE PROCEDURE INSERT_EMP(num_emp in integer, nom_emp in VARCHAR, prenom_emp in VARCHAR, adresse_emp in VARCHAR,tel_emp in VARCHAR, code_service in VARCHAR,rotation in VARCHAR ,salaire in integer ) AS
+	cursor cr is select * from infirmier; 
+	c_rec cr%rowtype; 
+	cursor cr1 is select * from employe; 
+	c_rec1 cr1%rowtype; 
+	cursor cr2 is select * from service;
+	c_rec2 cr2%rowtype;
+
+	Erreur1 integer; 
+	Erreur2 integer;
+	Erreur3 integer;
+BEGIN
+	Erreur1:=0; Erreur2:=0; Erreur3:=1;
+	FOR c_rec in cr LOOP 
+		IF(c_rec.num_inf=num_emp) THEN
+			Erreur1:=1;
+		ELSE 
+			FOR c_rec1 in cr1 LOOP
+				IF(c_rec1.num_emp=num_emp)THEN
+					Erreur2:=1;
+				ELSE 	
+					FOR c_rec2 in cr2 LOOP
+						IF(c_rec2.code_service=code_service)THEN
+							Erreur3:=0;
+						END IF;
+					END LOOP;	
+				END IF;
+			END LOOP;
+		END IF;
+	END LOOP;
+	IF(Erreur1=0 AND Erreur2=0 AND Erreur3=0) THEN
+			INSERT INTO employe VALUES(num_emp,nom_emp,prenom_emp,adresse_emp,tel_emp);
+			INSERT INTO infirmier VALUES(num_emp,code_service,rotation,salaire);
+	END IF;
+	IF(Erreur1=1)THEN 
+		dbms_output.put_line('l infirmier existe deja');
+	END IF;
+	IF(Erreur2=1)THEN
+		dbms_output.put_line('l employe existe deja');
+	END IF;
+	IF(Erreur3=1)THEN
+		dbms_output.put_line('le service n existe pas');
+	END IF;
+END INSERT_EMP;
